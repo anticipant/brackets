de8f0ddd2a0ffa1e7244cd03f185be7bf7490215
anticipant <shakhovich.d@yandex.ru>
2019-03-24 19:02:04 +0300

    implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let array = [];
+
+    // собираю массив конфига
+    for (let i = 0; i < bracketsConfig.length; i++ ) {
+        array[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+    }
+
+    // console.log(array);
+    let length = str.length;
+
+    for (let i = 0; i < length / 2; i++){
+
+        for (let i = 0; i < array.length; i++) {
+            let index = str.indexOf(array[i]);
+            if (index >= 0) {
+                str = str.substring(0,index) + str.substring(index + 2);
+            }
+        }
+
+    }
+
+    return !str.length > 0;
+};
